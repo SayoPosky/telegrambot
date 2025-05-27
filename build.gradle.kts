bac4d28abe94b5db86d04ff41320c0cedb651fd3
Ogunkile Oluwasayo <Ogunkileoluwasayo@gmail.com>
2025-05-27 17:06:22 +0100

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("java")
+    java
 }
 
 group = "com.ogunkile"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -12,10 +12,26 @@ repositories {
 dependencies {
     implementation("org.telegram:telegrambots:6.5.0")
     implementation("org.telegram:telegrambots-spring-boot-starter:6.5.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.ogunkile.telegrambot.SayoBotApp"
+    }
+
+    // Bundle all dependencies (fat jar)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from({
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
 }
